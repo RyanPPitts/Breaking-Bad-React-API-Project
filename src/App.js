6dbc26636930c9d7f812aba699be6ea4c12bebf6
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-07-19 20:09:00 -0700

    Header Component
    Created header componet that will house the page logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/ui/header';
 
 const App = () => {
   return (
     <div className="container">
-      <p>Hello</p>
+      <Header />
     </div>
   );
 };
